d7384bd4b2adc555b130790ac87e86bf0f08aefe
Alvaro Ramirez(Huananzhi-Xeon-Win10) <alvaro@ciencias.unam.mx>
2024-02-14 01:09:07 -0600

    Base de proyecto Chat-Bot creada

---
+++ Proyectos/film_finder_bot/bot.py
@@ -0,0 +1,38 @@
+import discord 
+import respuestas
+import os
+from dotenv import load_dotenv
+
+async def send_message(message, user_message, is_private):
+    response = respuestas.get_response(user_message)
+    await message.author.send(response) if is_private else await message.channel.send(response)
+    
+def run_discord_bot():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    
+    @client.event
+    async def on_ready():
+        print(f'{client.user} -------- Film Finder Bot Está vivo --------')
+        
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        
+        print(f'{username} en {channel} dijo: {user_message}')
+        
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private = True)
+        else:
+            await send_message(message, user_message, is_private = False)
+            
+    
+    client.run(TOKEN)
